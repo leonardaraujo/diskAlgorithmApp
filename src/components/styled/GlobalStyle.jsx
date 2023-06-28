21a0dcbba03963e719a42709f7acd29f9c6a0f09
Leonardo Araujo <leonardoaraujo.oct@gmail.com>
2023-06-28 18:44:02 -0500

    added sans-serif font

--- src/components/styled/GlobalStyle.jsx
+++ src/components/styled/GlobalStyle.jsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     *{
-        font-family:"quicksand";
+        font-family:"quicksand",sans-serif;
         box-sizing:border-box;
         margin:0;
         padding:0;
